f90520175352001b0a8cf39d78b34d312ea551fc
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2023-01-13 13:47:21 -0300

    Update mintsplash.sh

--- mintsplash.sh
+++ mintsplash.sh
@@ -6,21 +6,21 @@ IMGFILE=$1
 
 if [ $IMGFILE != "restore" ]; then
 
-if [ ! -d "$SPLASH_PATH_BAK" ]; then
-    sudo cp $SPLASH_PATH $SPLASH_PATH_BAK
-fi
+    if [ ! -d "$SPLASH_PATH_BAK" ]; then
+        sudo cp $SPLASH_PATH $SPLASH_PATH_BAK
+    fi
 
-for splashimg in $SPLASH_PATH/nombre*.png
-do rm -rf $splashimg && cp $IMGFILE $splashimg
-done
+    for splashimg in $SPLASH_PATH/nombre*.png
+        do rm -rf $splashimg && cp $IMGFILE $splashimg
+    done
 
-for splashimg in $SPLASH_PATH/nombre*.png
-do rm -rf $splashimg && cp $IMGFILE $splashimg
-done
+    for splashimg in $SPLASH_PATH/nombre*.png
+        do rm -rf $splashimg && cp $IMGFILE $splashimg
+    done
 
 else
-  sudo rm -rf $SPLASH_PATH
-  sudo cp $SPLASH_PATH_BAK $SPLASH_PATH 
+    sudo rm -rf $SPLASH_PATH
+    sudo cp $SPLASH_PATH_BAK $SPLASH_PATH 
 fi
 
 sudo update-initramfs -u
