0cb3f4702d4fd3a5bae812f1923f10291e440bd2
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2023-01-27 07:11:05 -0300

    Update mintsplash.sh

--- mintsplash.sh
+++ mintsplash.sh
@@ -12,20 +12,21 @@ if [ $IMGFILE != "restore" ]; then
     fi
     
     # Linux Mint
-    for splashimg in $SPLASH_PATH/nombre*.png
-        do sudo rm -rf $splashimg && sudo cp $IMGFILE $splashimg
-    done
-
-    for splashimg in $SPLASH_PATH/nombre*.png
-        do sudo rm -rf $splashimg && sudo cp $IMGFILE $splashimg
-    done
+    if [ -f "$SPLASH_PATH/animation-0001.png" ]; then
+        for splashimg in $SPLASH_PATH/animation*.png
+            do sudo rm -rf $splashimg && sudo cp $IMGFILE $splashimg
+        done
+        for splashimg in $SPLASH_PATH/throbber*.png
+            do sudo rm -rf $splashimg && sudo cp $IMGFILE $splashimg
+        done    
+    fi
     
     # LMDE
-    sudo rm -rf $SPLASH_PATH/logo.png
-    sudo cp $IMGFILE $SPLASH_PATH/logo.png
-    sudo rm -rf $SPLASH_PATH/spinner.png
-    sudo cp transparent.png $SPLASH_PATH/spinner.png
-
+    if [ -f "$SPLASH_PATH/logo.png" ]; then
+        sudo rm -rf $SPLASH_PATH/logo.png && sudo cp $IMGFILE $SPLASH_PATH/logo.png
+        sudo rm -rf $SPLASH_PATH/spinner.png && sudo cp transparent.png $SPLASH_PATH/spinner.png
+    fi
+    
 else
     sudo rm -rf $SPLASH_PATH
     sudo cp $SPLASH_PATH_BAK $SPLASH_PATH 
